5db10a55b0e7ac7ac746ae39a1bed8315f52a5ba
Pabline Ribeiro <119081902+pablineribeiro@users.noreply.github.com>
2023-06-05 11:45:24 -0300

    Add files via upload
    imprimir hello word+nome

---
+++ Wellcome.java
@@ -0,0 +1,11 @@
+
+package Exerciciosjava;
+
+public class Wellcome {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World, Pabline!");
+    }
+    
+}
